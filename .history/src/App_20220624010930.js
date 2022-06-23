11eabf713e4f3f818efdc9f99a195fd404d2b011
unknown <dangtoanminh875@gmail.com>
2022-06-24 02:50:23 +0700

    push

---
+++ .history/src/App_20220624010930.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import $ from "jquery";
+
+function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [posts, setPosts] = useState([]);
+
+  const pages = new Array(numberOfPages).fill(null).map((v, i) => i);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?page=${pageNumber}&results=${limit}
+ 
+`)
+      .then((response) => response.json())
+      .then(({ info, results }) => {
+        setPosts(results);
+        setNumberOfPages(10);
+      });
+  }, [pageNumber, limit]);
+
+  const gotoPrevious = () => {
+    setPageNumber(Math.max(1, pageNumber - 1));
+  };
+
+  const gotoNext = () => {
+    setPageNumber(Math.min(numberOfPages, pageNumber + 1));
+  };
+
+  return (
+    <div className="App">
+      <h3>Page of {pageNumber}</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post._id}>
+              <td>
+                {post.name.title + " " + post.name.first + " " + post.name.last}
+              </td>
+              <td>{post.name.first + " " + post.name.last}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button onClick={gotoPrevious}>Previous</button>
+      {pages.map((pageIndex) => (
+        <button key={pageIndex} onClick={() => setPageNumber(pageIndex + 1)}>
+          {pageIndex + 1}
+        </button>
+      ))}
+      <button onClick={gotoNext}>Next</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20220624021123.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+
+function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [posts, setPosts] = useState([]);
+
+  const columns = [
+    {
+      dataField: "fullname",
+      text: "Full Name",
+      sort: true,
+      sortCaret: sortCaret,
+    },
+    {
+      dataField: "username",
+      text: "User Name",
+      sort: true,
+      sortCaret: sortCaret,
+    },
+  ];
+  const pages = new Array(numberOfPages).fill(null).map((v, i) => i);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?page=${pageNumber}&results=${limit}
+ 
+`)
+      .then((response) => response.json())
+      .then(({ info, results }) => {
+        setPosts(
+          results.map((rs) => {
+            return {
+              fullname:
+                rs.name.title + " " + rs.name.first + " " + rs.name.last,
+              username: rs.name.first + " " + rs.name.last,
+            };
+          })
+        );
+        setNumberOfPages(10);
+      });
+  }, [pageNumber, limit]);
+
+  const gotoPrevious = () => {
+    setPageNumber(Math.max(1, pageNumber - 1));
+  };
+
+  const gotoNext = () => {
+    setPageNumber(Math.min(numberOfPages, pageNumber + 1));
+  };
+
+  return (
+    <div className="App">
+      <h3>Page of {pageNumber}</h3>
+      <BootstrapTable keyField="id" data={posts} columns={columns} />
+
+      <button onClick={gotoPrevious}>Previous</button>
+      {pages.map((pageIndex) => (
+        <button key={pageIndex} onClick={() => setPageNumber(pageIndex + 1)}>
+          {pageIndex + 1}
+        </button>
+      ))}
+      <button onClick={gotoNext}>Next</button>
+    </div>
+  );
+}
+
+export default App;
